5c15351fc7c1a2b96ac9ea11d0e656ef4f5f561c
vladimir.kostukov <vladimir.kostukov@8066c50f-1ede-328d-c459-0af97839c479>
2011-05-17 07:40:27 +0000

    SnoopyMM already works. And it works fine.

--- snoopymm/trunk/src/com/googlecode/snoopymm/snoopymm.py
+++ snoopymm/trunk/src/com/googlecode/snoopymm/snoopymm.py
@@ -18,51 +18,91 @@ import sys
 import Ice
 import os
 import re
+from xml.dom.minidom import parse
+
 import com.googlecode.snoopymm
 
 class ModuleManager(com.googlecode.snoopymm.IModuleManager): 
-	def __init__(self, path):
+
+	def __init__(self, path, config):
 		self.modulesDir = path
-
-	def deploy(self, muid, code, current=None):
-		pass
-	
-	def undeploy(self, muid, current=None):
-		pass
-
+		self.modulesConfig = config
+		
+		self.modules = self.fetch()
+		
 	def fetch(self, current=None):
 		modules = {}
-		files = os.listdir(self.modulesDir)
-		for file in files:
-			if (file.split(".")[1] == "py"):
-				muid = file.split(".")[0]
-				for line in open(os.path.join(self.modulesDir, file), "r").readlines():
-					if line.find("(object)"):
-						classes = line.split("(object)")[0].split(" ") 
-						if len(classes) == 2:
-							modules[muid] = classes[1]
-							break
-
+				
+		xml = parse(self.modulesConfig)
+		for module in xml.getElementsByTagName("module"):
+			muid = module.attributes["muid"].nodeValue
+			clazz = module.attributes["class"].nodeValue
+			modules[muid] = clazz
+			
 		return modules
 
+	def deploy(self, muid, code, current=None):
+		script = open(os.path.join(self.modulesDir, muid + ".py"), "w")
+		script.write(code)
+		script.close()
+		
+		clazz = re.match(r"class (\w+)\(.*\):", code)
+		clazz = clazz.group(1)
+				
+		xml = parse(self.modulesConfig)
+		modules = xml.getElementsByTagName("modules")[0]
+		module = xml.createElement("module")
+		module.setAttribute("muid", muid)
+		module.setAttribute("class", clazz)
+		module.appendChild(xml.createElement("params"))
+		modules.appendChild(module)
+		
+		modulesConfig = open(self.modulesConfig, "w") 
+		modulesConfig.write(xml.toxml())
+		modulesConfig.close()
+		
+	def undeploy(self, muid, current=None):
+		os.remove(os.path.join(self.modulesDir, muid + ".py"))
+		
+		moduleFound = False
+		xml = parse(self.modulesConfig)
+		modules = xml.getElementsByTagName("modules")[0]
+		for module in modules.getElementsByTagName("module"):
+			if module.attributes["muid"].nodeValue == muid:
+				modules.removeChild(module)
+				moduleFound = True
+				
+		
+		if moduleFound:
+			modulesConfig = open(self.modulesConfig, "w") 
+			modulesConfig.write(xml.toxml())
+			modulesConfig.close()
+		else:
+			print "module not found"
+
 	def launch(self, muid, params, current=None):
-		moduleName = self.fetch()[muid]
+		moduleName = self.modules[muid]
+		
 		dyncode = "module = __import__(muid); " + "module = module." + moduleName + "(); " + "result = module.invoke(params); "   
 		exec(dyncode)
+		
 		return result
-    
+
 class Snoopymm(Ice.Application):
+	
 	def run(self, args):
-		moduleManager = ModuleManager(self.communicator().getProperties().getProperty("Snoopy.ModulesDir"))
+		properties = self.communicator().getProperties()
+		moduleManager = ModuleManager(properties.getProperty("Snoopy.ModulesDir"), properties.getProperty("Snoopy.ModulesConfig"))
 		adapter = self.communicator().createObjectAdapter("Adapter")
 		adapter.add(moduleManager, self.communicator().stringToIdentity("ModuleManager"))
-	        adapter.activate()
+		adapter.activate()
 		self.communicator().waitForShutdown()
 		self.communicator().destroy()
+	
 		return 0
-    
+
 def main(args):
 	snoopymm = Snoopymm();
- 	sys.exit(snoopymm.main(args[1:], args[1]))
+	sys.exit(snoopymm.main(args[1:], args[1]))
 
 if __name__ == "__main__": main(sys.argv)
