d195788e748917dac967d2da4b0ff4ac4a1a222e
vladimir.kostukov <vladimir.kostukov@8066c50f-1ede-328d-c459-0af97839c479>
2011-05-17 07:41:20 +0000

    Snoopy.py was added.

--- snoopyapi/trunk/src/com/googlecode/snoopyapi/Snoopy.py
+++ snoopyapi/trunk/src/com/googlecode/snoopyapi/Snoopy.py
@@ -13,3 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+class Module(object):
+	
+	def invoke(self, params):
+		raise NotImplementedError();
+	
+	
