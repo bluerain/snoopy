4db1d387852a2bc16fa2b3239c5702188c2d75ce
vladimir.kostukov <vladimir.kostukov@8066c50f-1ede-328d-c459-0af97839c479>
2011-05-19 17:03:49 +0000

    MySQL jar was added in classpath

--- snoopyd/trunk/scripts/snoopyd.cmd
+++ snoopyd/trunk/scripts/snoopyd.cmd
@@ -4,7 +4,7 @@ rem
 rem  Copyrigth 2011, Snoopy Project
 rem 
 
-set CP=lib/snoopyd.jar;lib/log4j-1.2.15.jar;lib/Ice.jar;config/log4j.properies;lib/sigar.jar;
+set CP=lib/snoopyd.jar;lib/log4j-1.2.15.jar;lib/Ice.jar;config/log4j.properies;lib/sigar.jar;lib/mysql-connector-java-5.1.16-bin.jar;
 set DEFINES=-Dsnoopyd.configuration=config/snoopyd.conf -Dlog4j.configuration=config/log4j.properties
 set LIB=native/;
 set ENTRY_POINT=com.googlecode.snoopyd.Launcher 
