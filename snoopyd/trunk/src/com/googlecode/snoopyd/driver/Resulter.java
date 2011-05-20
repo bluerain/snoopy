8662ff11a1e4eab5819bb4eadc65cd86eed0c333
vladimir.kostukov <vladimir.kostukov@8066c50f-1ede-328d-c459-0af97839c479>
2011-05-20 07:12:07 +0000

    Resulter already works. Db support was implemented.

--- snoopyd/trunk/src/com/googlecode/snoopyd/driver/Resulter.java
+++ snoopyd/trunk/src/com/googlecode/snoopyd/driver/Resulter.java
@@ -15,19 +15,21 @@
  */
 package com.googlecode.snoopyd.driver;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 import org.apache.log4j.Logger;
-
-import Ice.Identity;
 
 import com.googlecode.snoopyd.Defaults;
 import com.googlecode.snoopyd.core.Kernel;
 import com.googlecode.snoopyd.core.state.ActiveState;
 import com.googlecode.snoopyd.core.state.KernelListener;
 import com.googlecode.snoopyd.core.state.KernelState;
-import com.googlecode.snoopyd.core.state.PassiveState;
 
 public class Resulter extends AbstractDriver implements Driver, Startable,
 		Runnable, KernelListener {
@@ -36,28 +38,36 @@ public class Resulter extends AbstractDriver implements Driver, Startable,
 
 	public static class Result {
 
-		private Ice.Identity identity;
-		private String muid;
+		private String hostname;
+		private String osname;
+		private String module;
 		private String[] result;
 
-		public Result(Identity identity, String muid, String[] result) {
-			this.identity = identity;
-			this.muid = muid;
+		public Result(String hostname, String osname, String module, String[] result) {
+			this.hostname = hostname;
+			this.osname = osname;
+			this.module = module;
 			this.result = result;
 		}
 
-		public Ice.Identity identity() {
-			return identity;
+		public String hostname() {
+			return hostname;
+		}
+		
+		public String osname() {
+			return osname;
 		}
 
-		public String muid() {
-			return muid;
+		public String module() {
+			return module;
 		}
 
 		public String[] result() {
 			return result;
 		}
 	}
+
+	private Connection connection;
 
 	private Queue<Result> pool;
 
@@ -70,10 +80,12 @@ public class Resulter extends AbstractDriver implements Driver, Startable,
 		this.started = false;
 	}
 
-	public synchronized void store(Ice.Identity identity, String muid,
+	public synchronized void store(String hostname, String osname, String module,
 			String[] result) {
+		
+		logger.debug("storing result " + Arrays.toString(result));
 
-		pool.offer(new Result(identity, muid, result));
+		pool.offer(new Result(hostname, osname, module, result));
 
 		if (pool.size() > Defaults.RESULTER_THRESHHOLD) {
 			notify();
@@ -84,6 +96,21 @@ public class Resulter extends AbstractDriver implements Driver, Startable,
 	public synchronized void start() {
 		logger.debug("starting " + name);
 
+		try {
+
+			String connectionurl = kernel.configuration().getProperty("connectionstring");
+			
+			logger.debug("use connection url: " + connectionurl);
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(connectionurl);
+
+		} catch (ClassNotFoundException ex) {
+			logger.error(ex.getMessage());
+		} catch (SQLException ex) {
+			logger.error(ex.getMessage());
+		}
+
 		Thread self = new Thread(this, Defaults.RESULTER_THREAD_NAME);
 		self.start();
 
@@ -93,6 +120,12 @@ public class Resulter extends AbstractDriver implements Driver, Startable,
 	@Override
 	public synchronized void stop() {
 		logger.debug("stoping " + name);
+
+		try {
+			connection.close();
+		} catch (SQLException ex) {
+
+		}
 
 		started = false;
 		notify();
@@ -121,7 +154,7 @@ public class Resulter extends AbstractDriver implements Driver, Startable,
 	public void run() {
 
 		for (; started;) {
-			
+
 			synchronized (this) {
 				try {
 					wait();
@@ -132,8 +165,22 @@ public class Resulter extends AbstractDriver implements Driver, Startable,
 			for (; !pool.isEmpty() && started;) {
 
 				Result result = pool.poll();
-				
-				// TODO: store in DB
+
+				try {
+					
+					StringBuilder sb = new StringBuilder();
+					for (String res: result.result()) {
+						sb.append(res);
+						sb.append(";");
+					}
+
+					Statement stmt = connection.createStatement();
+					stmt.executeUpdate(String.format("Call storeResult(\"%s\", \"%s\", \"%s\", \"%s\");", result.osname(), result.hostname(), result.module(), sb.toString()));
+					stmt.close();
+
+				} catch (SQLException ex) {
+					logger.debug(ex.getMessage());
+				}
 			}
 		}
 
@@ -144,8 +191,7 @@ public class Resulter extends AbstractDriver implements Driver, Startable,
 
 	@Override
 	public void stateChanged(KernelState currentState) {
-		if (currentState instanceof ActiveState
-				|| currentState instanceof PassiveState) {
+		if (currentState instanceof ActiveState) {
 			if (!started) {
 				start();
 			}
