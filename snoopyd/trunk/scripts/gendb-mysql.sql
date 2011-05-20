6902757232bebf8f4d1a4c7b9bb12e8e7a74c5c9
vladimir.kostukov <vladimir.kostukov@8066c50f-1ede-328d-c459-0af97839c479>
2011-05-20 06:26:48 +0000

    new correct gendb-mysql.sql with storaged procedure.

--- snoopyd/trunk/scripts/gendb-mysql.sql
+++ snoopyd/trunk/scripts/gendb-mysql.sql
@@ -14,6 +14,11 @@
  * limitations under the License.
 */
 
+Drop database If Exists snoopydb;
+
+Create Database snoopydb Character Set utf8 Collate utf8_general_ci;
+
+Use snoopydb;
 
 Create table Module (
 	idModule Int NOT NULL AUTO_INCREMENT,
@@ -24,7 +29,8 @@ Create table Result (
 	idResult Int NOT NULL AUTO_INCREMENT,
 	idHost Int NOT NULL,
 	idModule Int NOT NULL,
-	Datestamp Date,
+	datestamp Date,
+	result Varchar(512),
  Primary Key (idResult)) ENGINE = InnoDB;
 
 Create table Host (
@@ -42,5 +48,48 @@ Create table Os (
 Alter table Result add Foreign Key (idModule) references Module (idModule) on delete cascade on update  restrict;
 Alter table Result add Foreign Key (idHost) references Host (idHost) on delete cascade on update  restrict;
 Alter table Host add Foreign Key (idOs) references Os (idOs) on delete  restrict on update  restrict;
+
+Delimiter $$
+
+Create Procedure storeResult(In osname Varchar(128), In hostname Varchar(128), In modulename Varchar(128), In result Varchar(512)) 
+Begin
+	Declare osIdCount Int;
+	Declare hostIdCount Int;
+	Declare moduleIdCount Int;
+
+	Declare osId Int;
+	Declare hostId Int;
+	Declare moduleId Int;
+	
+	Select Count(*) Into osIdCount From Os Where name=osname;
+	If osIdCount = 0 Then
+		Insert Into Os(name) Values(osname);
+		Set osId = LAST_INSERT_ID();	
+	Else
+		Select idOs Into osId From Os Where name=osname;
+	End If;
+
+	Select Count(*) Into hostIdCount From Host Where name=hostname;
+	If hostIdCount = 0 Then
+		Insert Into Host(idOs, name) Values(osId, hostname);
+		Set hostId = LAST_INSERT_ID();	
+	Else
+		Select idHost Into hostId From Host Where name=hostname;
+	End If;
+
+	Select Count(*) Into moduleIdCount From Module Where name=modulename;
+	If moduleIdCount = 0 Then
+		Insert Into Module(name) Values(modulename);
+		Set moduleId = LAST_INSERT_ID();	
+	Else
+		Select idModule Into moduleId From Module Where name=modulename;
+	End If;
+
+	Insert Into Result(idHost, idModule, datestamp, result) Values(hostId, moduleId, CURDATE(), result);
+End;
+
+$$
+
+Delimiter ;
 
 
